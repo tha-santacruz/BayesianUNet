66f8704757040f21f3425093d106d42aadb0aea4
= <=>
2022-03-29 16:02:12 +0200

    adapt optimisation_routine

--- optimisation_routine.py
+++ optimisation_routine.py
@@ -1,3 +1,9 @@
+import argparse
+import logging
+import sys
+from pathlib import Path
+from datetime import datetime
+
 from train import train_net
 
 
@@ -20,4 +26,43 @@ amp = False
 optimizer = [Adam(weight_decay), SDGD(momentum), RMSprop]
 
 #TODO : to iterate to 
-momentum = 
+#momentum = 
+
+#TODO : defining the model load (if defined, it is the path for the *.pth file)
+load = False
+
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+logging.info(f'Using device {device}')
+
+# Change here to adapt to your data
+# n_channels=3 for RGB images
+# n_classes is the number of probabilities you want to get per pixel
+net = UNet(n_channels=7, n_classes=9, bilinear=args.bilinear)
+
+logging.info(f'Network:\n'
+                f'\t{net.n_channels} input channels\n'
+                f'\t{net.n_classes} output channels (classes)\n'
+                f'\t{"Bilinear" if net.bilinear else "Transposed conv"} upscaling')
+
+if load:
+    net.load_state_dict(torch.load(load, map_location=device))
+    logging.info(f'Model loaded from {load}')
+
+# 1. Create dataset
+dataset = BBKDataset(zone = ("genf", "goesch","jura"), split = "train", buildings = True, vegetation = True, random_seed = 1)
+
+net.to(device=device)
+try:
+    train_net(net=net,
+                epochs=epochs,
+                batch_size=batch_size,
+                learning_rate=learning_rate,
+                device=device,
+                val_percent=val / 100,
+                amp=amp)
+except KeyboardInterrupt:
+    torch.save(net.state_dict(), 'INTERRUPTED.pth')
+    logging.info('Saved interrupt')
+    sys.exit(0)
