b36419a3f181095178479fa44dbee7c5afca13d5
= <=>
2022-04-03 18:16:16 +0200

    adapt run_opt.sh to test different lr/patience

--- run_opti.sh
+++ run_opti.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
-python train.py -e 30 -b 32 -l 1e-2 -p 20 -o SGD 
-python train.py -e 30 -b 32 -l 1e-2 -p 30 -o SGD 
-python train.py -e 30 -b 32 -l 1e-2 -p 40 -o SGD 
+python train.py -e 20 -b 32 -l 1e-1 -p 2 -o SGD 
+python train.py -e 20 -b 32 -l 1e-1 -p 10 -o SGD 
+python train.py -e 20 -b 32 -l 1e-1 -p 20 -o SGD 
+python train.py -e 20 -b 32 -l 1e-1 -p 50 -o SGD 
+
+python train.py -e 20 -b 32 -l 1e-2 -p 2 -o SGD 
+python train.py -e 20 -b 32 -l 1e-2 -p 10 -o SGD 
+python train.py -e 20 -b 32 -l 1e-2 -p 20 -o SGD 
+python train.py -e 20 -b 32 -l 1e-2 -p 50 -o SGD 
+
+python train.py -e 20 -b 32 -l 1e-3 -p 2 -o SGD 
+python train.py -e 20 -b 32 -l 1e-3 -p 10 -o SGD 
+python train.py -e 20 -b 32 -l 1e-3 -p 20 -o SGD 
+python train.py -e 20 -b 32 -l 1e-3 -p 50 -o SGD 
