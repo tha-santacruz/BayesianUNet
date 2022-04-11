7e47666957a089a5947d32a8075a38549bb74531
= <=>
2022-04-11 13:38:08 +0200

    add bash files

--- run_opti5.sh
+++ run_opti5.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+
 python train.py -e 15 -b 32 -l 1e-2 -p 20 -o SGD --amp True
-python train.py -e 15 -b 32 -l 1e-2 -p 20 -o SGD --bilinear True
-python train.py -e 15 -b 32 -l 1e-2 -p 20 -o SGD --amp True --bilinear True
+python train.py -e 15 -b 32 -l 1e-2 -p 20 -o SGD --bilinear store_true
+python train.py -e 15 -b 32 -l 1e-2 -p 20 -o SGD --amp store_true --bilinear store_true
--- run_opti6.sh
+++ run_opti6.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-
-python train.py -e 40 -b 32 -l 1e-2 -p 20 -o SGD 
+sleep 9h
+python train.py -e 100 -b 32 -l 1e-2 -p 25 -o SGD 
