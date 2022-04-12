1c76ba379b0d8a6d4fbcf24ede32e9d643e9124c
tha-santacruz <nicola.santacroce@epfl.ch>
2022-04-12 13:21:24 +0200

    edited gt histogram script

--- gt_histogram.py
+++ gt_histogram.py
@@ -2,10 +2,18 @@ from utils.dataset import BBKDataset
 import torch
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-bbkd = BBKDataset(zone = ("goesch",),split="all", augment=True)
+bbkd = BBKDataset(zone = ("alles",),split="all", augment=True)
 dl = DataLoader(bbkd, batch_size=64, shuffle=True)
-x = next(iter(dl))
-output, counts = torch.unique(x[1], return_counts=True)
-print(output)
-print(counts)
+counts_cumulated = torch.tensor([0,0,0,0,0,0,0,0,0])
+for x in tqdm(dl):
+    output, counts = torch.unique(x[1].argmax(dim=1), return_counts=True)
+    for i in range(len(output)):
+        counts_cumulated[output[i]] += counts[i]
+counts_cumulated = counts_cumulated.div(counts_cumulated.sum())
+print(counts_cumulated)
+plt.bar(height=counts_cumulated.numpy(), x = bbkd.BBK_CLASSES_list)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
