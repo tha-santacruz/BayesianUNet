71af461891be6e262ff0d09968a0cf7ced5535fa
= <=>
2022-04-12 11:18:44 +0200

    try to fix the visualisation bug (not success)

--- train.py
+++ train.py
@@ -177,7 +177,9 @@ def train_net(net,
                         columns_table= list(class_labels.values())
                         data_table = [accuracy_per_class, F1_score]
                         score_table = wandb.Table(data = data_table, columns=columns_table)
-                        wandb_image = images[0][:5,:,:]*train_set.std_vals_tiles+train_set.mean_vals_tiles[:3,0,0]
+                        wandb_image = images[0].cpu()
+                        wandb_image = wandb_image[:5,:,:]*train_set.std_vals_tiles+train_set.mean_vals_tiles
+                        wandb_image = wandb_image[:3,:,:]
 
                         experiment.log({
                             'learning rate': optimizer.param_groups[0]['lr'],
@@ -193,7 +195,7 @@ def train_net(net,
                             'step': global_step,
                             'epoch': epoch,
                             'conf_mat' : wandb.Image(plt),
-                            'predictions': wb_mask(wandb_image.cpu(),
+                            'predictions': wb_mask(wandb_image,
                                                    torch.softmax(masks_pred, dim=1).argmax(dim=1)[0].cpu().numpy(),
                                                    torch.softmax(true_masks, dim=1).argmax(dim=1)[0].cpu().numpy(),
                                                    class_labels
--- utils/dataset.py
+++ utils/dataset.py
@@ -12,7 +12,7 @@ import albumentations as A
 
 class BBKDataset:
     def __init__(self, zone: tuple = ("alles",), split: str = "train", random_seed = 1,
-                 buildings: bool = True, vegetation: bool = True, augment: bool = False):
+                 buildings: bool = True, vegetation: bool = True, augment: bool = True):
         """Initialization method"""
         # Constants
         self.ROOT = "./../data/"
