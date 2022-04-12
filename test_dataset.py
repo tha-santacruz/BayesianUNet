7933c8f787d88e0ab8ca17cb20165f66d2172503
= <=>
2022-04-12 10:09:00 +0200

    imshow

--- test_dataset.py
+++ test_dataset.py
@@ -15,3 +15,14 @@ rgbidsm = document[:5,:,:]*bbkd.std_vals_tiles+bbkd.mean_vals_tiles
 image = rgbidsm[:3,:,:].div(torch.max(rgbidsm[:3,:,:])).permute(1,2,0).numpy()
 
 plt.imsave("test_image.png",image)
+
+# j = 10
+#     f, axarr = plt.subplots(4,2)
+#     axarr[0,0].imshow(noisy_imgs_1[4*j].permute(1,2,0).numpy())
+#     axarr[0,1].imshow(noisy_imgs_2[4*j].permute(1,2,0).numpy())
+#     axarr[1,0].imshow(noisy_imgs_1[4*j+1].permute(1,2,0).numpy())
+#     axarr[1,1].imshow(noisy_imgs_2[4*j+1].permute(1,2,0).numpy())
+#     axarr[2,0].imshow(noisy_imgs_1[4*j+2].permute(1,2,0).numpy())
+#     axarr[2,1].imshow(noisy_imgs_2[4*j+2].permute(1,2,0).numpy())
+#     axarr[3,0].imshow(noisy_imgs_1[4*j+3].permute(1,2,0).numpy())
+#     axarr[3,1].imshow(noisy_imgs_2[4*j+3].permute(1,2,0).numpy())
