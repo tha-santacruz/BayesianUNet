bbc078e2781fb4f82ebfa6e8218794c8f118d61a
tha-santacruz <nicola.santacroce@epfl.ch>
2022-04-12 13:41:07 +0200

    update

--- bayesian_unet/bayesian_unet_model.py
+++ bayesian_unet/bayesian_unet_model.py
@@ -3,9 +3,9 @@
 from .bayesian_unet_parts import *
 
 
-class UNet(nn.Module):
+class BayesianUNet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=False):
-        super(UNet, self).__init__()
+        super(BayesianUNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
