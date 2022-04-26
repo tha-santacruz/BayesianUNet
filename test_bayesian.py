d19b6468e1c636d0742639635ca3656ff2fe554e
= <=>
2022-04-26 17:04:53 +0200

    clean the code

--- test_bayesian.py
+++ test_bayesian.py
@@ -15,10 +15,17 @@ import numpy as np
 from tqdm import tqdm
 from sklearn.metrics import confusion_matrix
 
+# Source to compute entropy and mutual information : 
 # https://stackoverflow.com/questions/63285197/measuring-uncertainty-using-mc-dropout-on-pytorch
-def evaluate_uncertainty(net, dataloader, device, nb_forward):
+def evaluate_uncertainty(net,
+                        dataloader,
+                        device,
+                        nb_forward):
     net.eval()
+    #Activate the dropout layers
     enable_dropout(net)
+
+    #Define the bash number
     num_val_batches = len(dataloader)
 
     #Initialization of scores
@@ -38,12 +45,12 @@ def evaluate_uncertainty(net, dataloader, device, nb_forward):
         image, mask_true = batch[0], batch[1]
         # move images and labels to correct device and type
         image = image.to(device=device, dtype=torch.float32)
-        mask_true = mask_true.to(#device=device,
-                                 dtype=torch.float32)
+        mask_true = mask_true.to(dtype=torch.float32)
 
-        # to store n_forward predictions on the same batch
+        #to store n_forward predictions on the same batch
         dropout_predictions = torch.empty((0,mask_true.size(0),mask_true.size(1),mask_true.size(2),mask_true.size(3)))
-
+        
+        # iterate over the forward passes
         for f_pass in range(nb_forward):
             with torch.no_grad():
                 # predict the mask (pytorch tensor have the following structure : [batch_no, class, pixel_x, pixel_y])
@@ -56,22 +63,19 @@ def evaluate_uncertainty(net, dataloader, device, nb_forward):
                 cf_matrix = cf_matrix + confusion_matrix(mask_true.argmax(dim=1).view(-1).cpu(),mask_pred.argmax(dim=1).view(-1).cpu(), labels = np.arange(0,9))
 
         
-        
-        #print(dropout_predictions.unique())
         # if torch.eq(dropout_predictions[-1].view(-1),dropout_predictions[-2].view(-1)).all():
         #     print("same result")
+
+        #Compute uncertainty indicateurs
         batch_mean = dropout_predictions.mean(dim=0)
         batch_std = dropout_predictions.std(dim=0)
-        #print(batch_std.unique())
-
-        #print(f"mean size {batch_mean.size()}")
+        
         batch_pred_entropy = -torch.sum(batch_mean*batch_mean.log(),dim=1)
-        #print(f"entropy size {batch_pred_entropy.size()}")
         batch_mutual_info = batch_pred_entropy+torch.mean(torch.sum(dropout_predictions*dropout_predictions.log(),dim=-3),dim=0)
+        
+        #print(f"mean size {batch_mean.size()}")
+        #print(f"entropy size {batch_pred_entropy.size()}")
         #print(f"mutual info size {batch_mutual_info.size()}")
-
-        #print(batch_pred_entropy.mean())
-        #print(batch_pred_entropy.mean(dim=(-2,-1)).size())
 
         #transform predictions to float labels for others metrics
         mask_pred = batch_mean
@@ -93,71 +97,63 @@ def evaluate_uncertainty(net, dataloader, device, nb_forward):
         dice_score += metrics.multiclass_dice_coeff(mask_pred[:, 1:, ...], mask_true[:, 1:, ...], reduce_batch_first=False)
 
 
-        ## produce PAVPU score
-        # hyperparameteters
+        #Produce PAVPU score
         w_size = 4 #patch size
         accuracy_tresh = 0.5
         
-        #traverse the matrix with a patch 
-        unfold = torch.nn.Unfold(kernel_size=(w_size, w_size),stride = 4) #to have 1 meter block
+        #Define the patch size
+        unfold = torch.nn.Unfold(kernel_size=(w_size, w_size),stride = w_size) #to have 1 meter block
 
-        #compute the accuracy each pach and check if it above the threshold
+        #compute the accuracy for each patch and check if it above the threshold
         masktrue_unfold = unfold(mask_true_labels.unsqueeze(dim=1).to(torch.float32))
         pred_unfold = unfold(mask_pred_labels.unsqueeze(dim=1).to(torch.float32))
+        accuracy_matrix = torch.eq(pred_unfold, masktrue_unfold).to(torch.float32).mean(dim=1)
+        bool_acc_matrix = torch.gt(accuracy_matrix, accuracy_tresh).to(torch.float32)
         #print('pred_unfold size: {}'.format(pred_unfold.size()))
         #print('masktrue_unfold size: {}'.format(masktrue_unfold.size()))
-        accuracy_matrix = torch.eq(pred_unfold, masktrue_unfold).to(torch.float32).mean(dim=1)
         #print('accuracy_matrix size :{}'.format(accuracy_matrix.size()))
-        #print('average accuracy : {}'.format(accuracy_matrix.mean()))
-        
+        #print('bool_acc_matrix size: {}'.format(bool_acc_matrix.size()))
 
-        # compute the mean uncertainty and if it above the threshold
+        # compute the mean uncertainty and if it is above the threshold
         uncertainty_matrix = unfold(batch_pred_entropy.unsqueeze(dim=1)).mean(dim=1)
+        #Define the uncertainty treshold as in the litterature
+        t = 0.4
+        uncertainty_tresh = uncertainty_matrix.min()+t*(uncertainty_matrix.max()-uncertainty_matrix.min())
+        bool_uncert_matrix = torch.gt(uncertainty_matrix, uncertainty_tresh).to(torch.float32)
+      
         #print('uncertainty_matrix size :{}'.format(uncertainty_matrix.size()))
-
-        bool_acc_matrix = torch.gt(accuracy_matrix, accuracy_tresh).to(torch.float32)
-        print('bool_acc_matrix size: {}'.format(bool_acc_matrix.size()))
-        
         #print('average uncertainty score: {}'.format(uncertainty_matrix.mean()))
         #print('std uncertainty score: {}'.format(uncertainty_matrix.std()))
         #print('range uncertainty score: {}'.format(uncertainty_matrix.max()-uncertainty_matrix.min()))
         
-        t = 0.4
-        uncertainty_tresh = uncertainty_matrix.min()+t*(uncertainty_matrix.max()-uncertainty_matrix.min())
-
-        bool_uncert_matrix = torch.gt(uncertainty_matrix, uncertainty_tresh).to(torch.float32)
-        #print('bool_acc_matrix size: {}'.format(bool_uncert_matrix.size()))
-
-        #compute cf matrix for both accuracy and uncertainity
-        # compute confidence matrix
         
-        #compute the PAVU score
+        #compute the performances metrics 
         nac = (bool_acc_matrix*(1-bool_uncert_matrix)).sum()
         nac_plus_nic = (1-bool_uncert_matrix).sum()
         niu = (bool_uncert_matrix*(1-bool_acc_matrix)).sum()
         nic_plus_niu = (1-bool_acc_matrix).sum()
-
         pa += nac/nac_plus_nic
         pu += niu/nic_plus_niu
         pavpu += (nac+niu) / torch.ones_like(bool_acc_matrix).sum()
         
-        '''
-        print('pa = {}'.format(pa))
-        print('pu = {}'.format(pu))
-        print('pavpu = {}'.format(pavpu))
+        
+        #print('pa = {}'.format(pa))
+        #print('pu = {}'.format(pu))
+        #print('pavpu = {}'.format(pavpu))
+        #print(torch.mean(bool_acc_matrix))
+        #print(torch.mean(bool_uncert_matrix))
 
-        print(torch.mean(bool_acc_matrix))
-        print(torch.mean(bool_uncert_matrix))
-        '''
+    #Compute the global confusion matrix    
     cf_matrix = cf_matrix/cf_matrix.sum(axis=1,keepdims=True)
 
-    net.train()
+    
 
     print('pa = {}'.format(pa/num_val_batches))
     print('pu = {}'.format(pu/num_val_batches))
     print('pavpu = {}'.format(pavpu/num_val_batches))
 
-
+    #Re-turn the model in train mode
+    net.train()
     return (dice_score / num_val_batches,
             accuracy_score/num_val_batches,
             accuracy_per_class/num_val_batches,
@@ -189,7 +185,7 @@ if __name__ == '__main__':
     logging.info(f'Using device {device}')
 
     # define test set
-    test_set = BBKDataset(zone = ("genf",), split = "test", buildings = True, vegetation = True, random_seed = 1)
+    test_set = BBKDataset(zone = ("alls",), split = "test", buildings = True, vegetation = True, random_seed = 1)
     test_dl = DataLoader(test_set, batch_size=32, shuffle=True)
 
     # declare model
@@ -210,13 +206,16 @@ if __name__ == '__main__':
     pu,
     pavpu) = evaluate_uncertainty(net,test_dl,device, nb_forward=10)
     
-    # create wandb objects for visualisation
+    #Create wandb objects for visualisation
+
+    ##Confusion matrix 
     plt.figure()
     sns.heatmap(cf_matrix, annot=True, annot_kws={"size":8}, fmt='.2%', cmap='Blues', cbar=True, xticklabels=test_set.BBK_CLASSES_list,yticklabels=test_set.BBK_CLASSES_list)
     plt.xticks(rotation=45)
     plt.yticks(rotation=45)
     plt.tight_layout()
 
+    ##Score table
     class_labels = {0 : "null",
                     1 : "wooded_area",
                     2 : "water",
@@ -234,10 +233,9 @@ if __name__ == '__main__':
     columns_table= list(class_labels.values())
     data_table = [accuracy_per_class, F1_score, IOU_score_per_class]
     score_table = wandb.Table(data = data_table, columns=columns_table)
-    #Add a column for scores names
-    score_table.add_column(name='score',data=list(scores.keys()))
+    score_table.add_column(name='score',data=list(scores.keys())) #Add a column for scores names
 
-
+    #Log the objects in wandb
     experiment.log({
                 'Validation Dice score': val_score,
                 'Global accuracy score': accuracy_score,
@@ -245,6 +243,5 @@ if __name__ == '__main__':
                 'PAvPU score':pavpu,
                 'Metric per class':score_table, 
                 'conf_mat' : wandb.Image(plt),
-                #**histograms
             })
     plt.close()
