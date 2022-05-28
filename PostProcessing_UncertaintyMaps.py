d5a382f5aea48936d223656d6bd7cdce6363a5a8
= <=>
2022-05-28 11:49:06 +0200

    modify post processing

--- PostProcessing_UncertaintyMaps.py
+++ PostProcessing_UncertaintyMaps.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 
 def map_whitout_edges(img:,
                     kernel_size : int = 5):
-
+    '''
+    Perform erosion operation to remove the edges on the uncertainty map
+    '''
     # Taking a matrix of size 5 as the kernel
     kernel = np.ones((kernel_size,kernel_size), np.uint8)
     img_erosion = cv2.erode(img, kernel, iterations=1)
@@ -14,6 +16,10 @@ def map_whitout_edges(img:,
 def binary_map(img:,
             kernel_size : int = 5):
 
+     '''
+    Perform a tresholding using Otsus method following 
+    by an erosion operation to create binary uncertainty map
+    '''
     # Taking a matrix of size 5 as the kernel
     kernel = np.ones((kernel_size,kernel_size), np.uint8)
 
